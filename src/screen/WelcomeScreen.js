73eaa64d911068fe7ac11abe417998d51b5e8db0
Helmi Aziz <helmiazizm@gmail.com>
2022-03-11 08:56:25 +0700

    Other screens

---
+++ src/screen/WelcomeScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import GLOBAL from '../global';
+
+const WelcomeScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texts}>Hello</Text>
+      <Image
+        source={require('../assets/Images/undraw_People_re_8spw.png')}
+        style={{height: 256, width: 256}}
+      />
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onNavigateLogin(navigation)}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const onNavigateLogin = onNavigate => onNavigate.navigate(GLOBAL.SCREEN.LOGIN);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  texts: {
+    fontSize: 36,
+    fontFamily: 'RobotoCondensed-Bold',
+  },
+  buttonView: {
+    alignSelf: 'stretch',
+  },
+  button: {
+    backgroundColor: '#D77FA1',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 20,
+    padding: 12,
+  },
+  buttonText: {
+    color: '#D6E5FA',
+    fontWeight: 'bold',
+    fontFamily: 'RobotoCondensed-Regular',
+    fontSize: 16,
+  },
+});
+// /home/user/FirstProject/src/assets/Images/undraw_People_re_8spw.png
+export default WelcomeScreen;
